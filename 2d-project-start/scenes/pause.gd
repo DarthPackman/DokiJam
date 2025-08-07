509a4f19fd2ba7a8ea99a8d943ec9e687f703254
DarthPackman <packman150@hotmail.com>
2025-08-06 22:47:43 -0700

    Pause

---
+++ 2d-project-start/scenes/pause.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@onready var pause_menu = $"."
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_toggle_pause()
+
+func _toggle_pause() -> void:
+	if pause_menu.visible:
+		pause_menu.hide()
+		get_tree().paused = false
+	else:
+		pause_menu.show()
+		get_tree().paused = true
+
+func _on_resume_button_pressed() -> void:
+	_toggle_pause()
+
+func _on_main_menu_button_pressed() -> void:
+	_toggle_pause()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
