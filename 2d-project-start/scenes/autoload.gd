fbd4a68c19f5ec147a4cbb2f799ca4d732e2ca03
DarthPackman <packman150@hotmail.com>
2025-08-12 14:23:31 -0700

    menu sounds

--- 2d-project-start/scenes/autoload.gd
+++ 2d-project-start/scenes/autoload.gd
@@ -4,20 +4,16 @@ var character_selected_name: String = ""
 var character_selected_player: PackedScene = null
 
 func button_click():
-	$Click.play()
+	$ClickASP.play()
 
 func button_hover():
-	$Hover.play()
+	$CursorASP.play()
 	
-func step_play():
-	var rng = RandomNumberGenerator.new()
-	var my_random_number = rng.randi_range(1, 3)
-	if my_random_number == 1:
-		$Step1.play()
-	elif my_random_number == 2:
-		$Step2.play()
-	else:
-		$Step3.play()
+func button_back():
+	$BackASP.play()
+	
+func button_confirm():
+	$ConfirmASP.play()
 
 func set_selected_character_data(name: String, player: PackedScene):
 	character_selected_name = name
--- 2d-project-start/scenes/titlescreen.gd
+++ 2d-project-start/scenes/titlescreen.gd
@@ -2,10 +2,16 @@ extends Control
 
 
 func _on_playbutton_pressed() -> void:
+	Autoload.button_click()
 	get_tree().change_scene_to_file("res://scenes/pickbird.tscn")
 
 func _on_optionsbutton_pressed() -> void:
+	Autoload.button_click()
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_quitbutton_pressed() -> void:
+	Autoload.button_click()
 	get_tree().quit()
+
+func _on_button_mouse_entered() -> void:
+	Autoload.button_hover()
