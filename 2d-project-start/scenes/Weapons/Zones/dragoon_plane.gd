ab662fc22fcb954ed2053ed293b2722331c753f9
DarthPackman <packman150@hotmail.com>
2025-08-12 18:16:39 -0700

    dragoon dro

---
+++ 2d-project-start/scenes/Weapons/Zones/dragoon_plane.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@onready var attackSpeedTimer = $Timer
+@export var attackSpeed = 1.5
+@export var statusEffectDisabled = false
+@export var duration = 1.0
+
+func _ready() -> void:
+	push_warning("Grenade Launcher")
+	attackSpeedTimer.wait_time = attackSpeed
+
+func _physics_process(delta: float) -> void:
+	var random_x = randi() % int(get_viewport_rect().size.x)
+	var random_y = randi() % int(get_viewport_rect().size.y)
+	var random_position = Vector2(random_x, random_y)
+	look_at(random_position)
+
+func shoot():
+	push_warning("Dragoon Dropped")
+	const ZONE = preload("res://scenes/Weapons/Zones/dragoondrop.tscn")
+	var new_zone = ZONE.instantiate()
+	new_zone.global_position = %LandingSpot.global_position
+	new_zone.disabled = statusEffectDisabled
+	get_tree().root.add_child(new_zone)
+
+func _on_timer_timeout() -> void:
+	shoot()
+
+func level_up():
+	pass
---
+++ 2d-project-start/scenes/Weapons/Zones/dragoondrop.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+var damage = 5
+var disabled = false
+var duration = 5.0
+var duration_elapsed = 0.0
+var interval = 0.5
+
+func _ready() -> void:
+	$Timer.wait_time = interval
+	$Dragoon.play("trigger")
+	$FX.play("trigger")
+	
+func _process(delta: float) -> void:
+	duration_elapsed += delta
+	if not $Dragoon.is_playing():
+		$Dragoon.play("duration")
+		$FX.play("duration")
+	if duration_elapsed >= duration:
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	var hit_mobs = $".".get_overlapping_bodies()
+	for enemy in hit_mobs:
+		if enemy.has_method("take_damage"):
+			var damageType = DamageNumbers.DamageTypes.NORMAL
+			enemy.take_damage(damage, damageType)
+			if not disabled:
+				StatusEffects.applyVulnerable(enemy)
