0600a57041facf0523c30ae182771a61d133c291
DarthPackman <packman150@hotmail.com>
2025-08-12 19:10:54 -0700

    smashed ground

---
+++ 2d-project-start/scenes/Weapons/Zones/stoppyboots.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@onready var attackSpeedTimer = $Timer
+@export var attackSpeed = 0.5
+@export var statusEffectDisabled = false
+@export var duration = 1.0
+
+func _ready() -> void:
+	attackSpeedTimer.wait_time = attackSpeed
+
+func _physics_process(delta: float) -> void:
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0.0:
+		var target_enemy = enemies_in_range.front()
+		look_at(target_enemy.global_position)
+
+func shoot():
+	push_warning("Grenade launched")
+	const ZONE = preload("res://scenes/Weapons/Zones/stoppyboots.tscn")
+	var new_zone = ZONE.instantiate()
+	new_zone.global_position = %LandingSpot.global_position
+	new_zone.global_rotation = %LandingSpot.global_rotation
+	new_zone.disabled = statusEffectDisabled
+	get_tree().root.add_child(new_zone)
+
+func _on_timer_timeout() -> void:
+	shoot()
+
+func level_up():
+	pass
