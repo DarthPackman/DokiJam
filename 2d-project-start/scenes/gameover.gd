c2cac7effa7d2b47006bf4f2ce4ed6aedc34be2a
hyperflair <42447055+hyperflair@users.noreply.github.com>
2025-08-14 07:30:40 -0700

    button clicks added

--- 2d-project-start/scenes/gameover.gd
+++ 2d-project-start/scenes/gameover.gd
@@ -2,8 +2,14 @@ extends Control
 
 
 func _on_retrybutton_pressed() -> void:
+	Autoload.button_click()
 	get_tree().change_scene_to_file("res://scenes/survivor_game.tscn")
 
 
 func _on_menubutton_pressed() -> void:
+	Autoload.button_click()
 	get_tree().change_scene_to_file("res://scenes/titlescreen.tscn")
+
+
+func _on_button_mouse_entered() -> void:
+	Autoload.button_hover()
