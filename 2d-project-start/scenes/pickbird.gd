fa4dce59c6589144a7d21580de7586ce0c376e3a
DarthPackman <packman150@hotmail.com>
2025-08-15 11:17:17 -0700

    tutorial

--- 2d-project-start/scenes/pickbird.gd
+++ 2d-project-start/scenes/pickbird.gd
@@ -8,25 +8,25 @@ func _on_selectreg_pressed() -> void:
 	Autoload.button_click()
 	playerScene = preload("res://scenes/PlayableCharacters/regular_dragoon.tscn")
 	Autoload.set_selected_character_data("Regular", playerScene, "m_r_WingSlap")
-	get_tree().change_scene_to_file("res://scenes/survivor_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_selectchonk_pressed() -> void:
 	Autoload.button_click()
 	playerScene = preload("res://scenes/PlayableCharacters/chonky_dragoon.tscn")
 	Autoload.set_selected_character_data("Chonk", playerScene, "z_ch_StoppyBoots")
-	get_tree().change_scene_to_file("res://scenes/survivor_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_selecttall_pressed() -> void:
 	Autoload.button_click()
 	playerScene = preload("res://scenes/PlayableCharacters/long_dragoon.tscn")
 	Autoload.set_selected_character_data("Long", playerScene,"r_lo_bow")
-	get_tree().change_scene_to_file("res://scenes/survivor_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_selectegg_pressed() -> void:
 	Autoload.button_click()
 	playerScene = preload("res://scenes/PlayableCharacters/egg_dragoon.tscn")
 	Autoload.set_selected_character_data("Egg", playerScene, "a_egg_RottenEgg")
-	get_tree().change_scene_to_file("res://scenes/survivor_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_back_pressed() -> void:
 	Autoload.button_click()
